8dddc40ebe64123c5595edac91b685d7fa961fb3
franciscoteston <franciscoteston@gmail.com>
2026-06-26 19:12:31 -0300

    fix: adiciona CSRF_TRUSTED_ORIGINS baseado em ALLOWED_HOSTS para resolver erro 403 no Railway

--- siprac/settings.py
+++ siprac/settings.py
@@ -30,6 +30,12 @@ DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
+CSRF_TRUSTED_ORIGINS = [
+    f'https://{host.strip()}'
+    for host in os.environ.get('ALLOWED_HOSTS', '').split(',')
+    if host.strip()
+]
+
 
 # Application definition
 
