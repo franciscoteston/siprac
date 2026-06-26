b78008fe9a02f294193242ef64b0c70d2bc1450d
franciscoteston <franciscoteston@gmail.com>
2026-06-26 18:52:03 -0300

    fix: usa caminho absoluto /opt/venv/bin para gunicorn e python no start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 set -o errexit
 
-source /opt/venv/bin/activate
-
-python manage.py migrate --noinput
-exec gunicorn siprac.wsgi --log-file - --bind 0.0.0.0:$PORT
+/opt/venv/bin/python manage.py migrate --noinput
+exec /opt/venv/bin/gunicorn siprac.wsgi --log-file - --bind 0.0.0.0:$PORT
