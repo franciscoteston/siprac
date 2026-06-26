2d34807269ec2489d00a9dfe36a288405e388447
franciscoteston <franciscoteston@gmail.com>
2026-06-26 19:05:50 -0300

    debug: adiciona echo de PORT e fallback para 8080 no start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 set -o errexit
 
-# Localiza o python correto (venv do Nixpacks ou do sistema)
 if [ -f "/opt/venv/bin/python" ]; then
     PYTHON="/opt/venv/bin/python"
 else
@@ -9,7 +8,7 @@ else
 fi
 
 echo "Usando Python: $PYTHON"
-$PYTHON -m pip show gunicorn || $PYTHON -m pip install gunicorn
+echo "PORT recebido: $PORT"
 
 $PYTHON manage.py migrate --noinput
-exec $PYTHON -m gunicorn siprac.wsgi --log-file - --bind 0.0.0.0:$PORT
+exec $PYTHON -m gunicorn siprac.wsgi --log-file - --bind 0.0.0.0:${PORT:-8080}
